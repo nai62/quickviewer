d292abbb4da874780f2195066e3095bc27ef4be8
hirano <nai.yh.62@gmail.com>
2026-09-03 11:08:27 +0900

    Clean up Windows window integration

--- QuickViewer/src/mainwindowforwindows.cpp
+++ QuickViewer/src/mainwindowforwindows.cpp
@@ -3,24 +3,13 @@
 #include <Shellapi.h>
 #include <QtCore>
 
-#include "qvapplication.h"
+#include <string>
+
 #include "mainwindowforwindows.h"
-#include "ui_mainwindow.h"
-
-#define IDM_SHOWMAINMENU 2
-
-MainWindowForWindows *MainWindowForWindows::MainWindowForWindows_self;
-WNDPROC MainWindowForWindows::DefStaticProc;
 
 MainWindowForWindows::MainWindowForWindows(QWidget *parent)
-    : MainWindow(parent),
-      bFirstView(true)
-{
-    QString menutxt = tr("Sh&ow/Hide MainMenuBar", "An additional menu displayed when right-clicking the title bar, which is used when undoing the main menu when it is hidden");
-    menutxt = ui->actionShowMainMenuBar->text();
-
-    MainWindowForWindows_self = this;
-}
+    : MainWindow(parent)
+{}
 
 bool MainWindowForWindows::moveToTrash(QString path)
 {
@@ -46,13 +35,8 @@ bool MainWindowForWindows::setStayOnTop(bool top)
     if (!hwnd) {
         return false;
     }
-    // Place window A on top
-    if (top) {
-        ::SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
-    } else {
-        ::SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
-    }
-    return true;
+    const HWND insertAfter = top ? HWND_TOPMOST : HWND_NOTOPMOST;
+    return ::SetWindowPos(hwnd, insertAfter, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE) != FALSE;
 }
 
 void MainWindowForWindows::setWindowTop(bool signalOnly)
@@ -61,55 +45,22 @@ void MainWindowForWindows::setWindowTop(bool signalOnly)
     if (!hwnd) {
         return;
     }
-    if (!isMaximized()) {
+    if (isMinimized()) {
         ::ShowWindow(hwnd, SW_RESTORE);
     }
     //    ::SwitchToThisWindow(hwnd, false);
-    int foregroundID;
-    if (!signalOnly) {
-        foregroundID = ::GetWindowThreadProcessId(::GetForegroundWindow(), NULL);
-        ::AttachThreadInput(::GetCurrentThreadId(), foregroundID, TRUE);
-    }
+    const DWORD currentThreadId = ::GetCurrentThreadId();
+    const DWORD foregroundThreadId = ::GetWindowThreadProcessId(::GetForegroundWindow(), nullptr);
+    const bool inputAttached = !signalOnly && foregroundThreadId != 0 && foregroundThreadId != currentThreadId && ::AttachThreadInput(currentThreadId, foregroundThreadId, TRUE);
 
     ::SetForegroundWindow(hwnd);
     ::SetActiveWindow(hwnd);
     ::SetFocus(hwnd);
     ::SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
 
-    if (!signalOnly) {
-        ::AttachThreadInput(::GetCurrentThreadId(), foregroundID, FALSE);
+    if (inputAttached) {
+        ::AttachThreadInput(currentThreadId, foregroundThreadId, FALSE);
     }
-}
-
-static LPSTR qStringToLPSTR(const QString &qString)
-{
-    // return qString.toLocal8Bit().data();
-    return qString.toLatin1().data();
-}
-
-static QString mapi32path()
-{
-    // // try to determine default Simple MAPI handler
-    // // first, look for current user's default client
-    // registry::tstring default_client =  registry::const_key(HKEY_CURRENT_USER, "Software\\Clients\\Mail")[""].reg_sz("");
-    //
-    // if (default_client.empty()) {
-    // 	// then look for machine-wide settings
-    // 	default_client =  registry::const_key(HKEY_LOCAL_MACHINE, "Software\\Clients\\Mail")[""].reg_sz("");
-    // }
-    //
-    // if (!default_client.empty()) {
-    // 	registry::const_key regClient(HKEY_LOCAL_MACHINE, registry::tstring("Software\\Clients\\Mail\\" + default_client).c_str());
-    // 	registry::tstring s = regClient["DLLPath"].reg_sz("");
-    //
-    // 	if (s.empty())
-    // 		s = regClient["DLLPathEx"].reg_sz("");
-    //
-    // 	if (!s.empty())
-    // 		dllpath = s;
-    // }
-
-    return "mapi32";
 }
 
 void MainWindowForWindows::setMailAttachment(QString path)
@@ -167,61 +118,8 @@ void MainWindowForWindows::setMailAttachment(QString path)
 
 bool MainWindowForWindows::eventFilter(QObject *obj, QEvent *event)
 {
-    switch (event->type()) {
-    case QEvent::Leave:
-        //        if(obj == ui->pageFrame && isFullScreen()) {
-        //            ui->pageFrame->hide();
-        //            return true;
-        //        }
+    if (event->type() == QEvent::Leave) {
         return QObject::eventFilter(obj, event);
-    default:
-        break;
     }
     return MainWindow::eventFilter(obj, event);
-}
-
-// // This method is used because QShowEvent may not be called
-// bool MainWindowForWindows::nativeEvent(const QByteArray &, void *, long *)
-// {
-//     if(!bFirstView)
-//         return false;
-
-// //    int mes = reinterpret_cast<int>(message);
-//     auto hwnd = reinterpret_cast<HWND>(winId());
-//     m_showMainMenu = ::GetSystemMenu(hwnd, false);
-
-//     QString menutxt = ui->actionShowMainMenuBar->text();
-
-//     ::AppendMenu(m_showMainMenu, MF_SEPARATOR, IDM_SHOWMAINMENU-1, 0);
-//     ::AppendMenu(m_showMainMenu, MF_STRING, IDM_SHOWMAINMENU,
-//                  reinterpret_cast<LPCWSTR>(menutxt.utf16()));
-
-// #ifdef _WIN64
-//     DefStaticProc = (WNDPROC)GetWindowLongPtr(hwnd , GWLP_WNDPROC);
-//     SetWindowLongPtr(hwnd , GWLP_WNDPROC , reinterpret_cast<LONG_PTR>(StaticProc));
-// #else
-//     DefStaticProc = (WNDPROC)GetWindowLongPtr(hwnd , GWL_WNDPROC);
-//     SetWindowLongPtr(hwnd , GWL_WNDPROC , reinterpret_cast<LONG_PTR>(StaticProc));
-// #endif
-//     bFirstView = false;
-//     return false;
-// }
-
-LRESULT CALLBACK MainWindowForWindows::StaticProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
-{
-    switch (msg) {
-    case WM_SYSCOMMAND:
-        if (LOWORD(wp) == IDM_SHOWMAINMENU) {
-            if (qApp->ShowMenuBar()) {
-                MainWindowForWindows_self->ui->actionShowMenuBar->setChecked(false);
-                MainWindowForWindows_self->ui->actionShowMenuBar->triggered(false);
-            } else {
-                MainWindowForWindows_self->ui->actionShowMenuBar->setChecked(true);
-                MainWindowForWindows_self->ui->actionShowMenuBar->triggered(true);
-            }
-            return 0;
-        }
-        break;
-    }
-    return CallWindowProc(DefStaticProc, hwnd, msg, wp, lp);
 }
