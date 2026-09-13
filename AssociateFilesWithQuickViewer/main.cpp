ca1d80bc22be18a7f98894067447669cb33d6cff
Yuichiro Hirano <31331343+nai62@users.noreply.github.com>
2026-09-13 11:41:48 +0900

    Update file association tool for language utility rename

--- AssociateFilesWithQuickViewer/main.cpp
+++ AssociateFilesWithQuickViewer/main.cpp
@@ -1,11 +1,11 @@
 #include "fileassocdialog.h"
 #include <QApplication>
-#include "qlanguageselector.h"
+#include "languagemanager.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QLanguageSelector selector("quickviewer_", "translations/");
+    LanguageManager selector("quickviewer_", "translations/");
     selector.resetTranslator("");
 
     FileAssocDialog w;
