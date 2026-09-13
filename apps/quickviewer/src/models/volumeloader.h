aa1b8b59de681f251b4f5f61b43fa7839f7ecaa5
hirano <31331343+nai62@users.noreply.github.com>
2026-09-14 01:09:03 +0900

    Restore archive loader documentation

--- apps/quickviewer/src/models/volumeloader.h
+++ apps/quickviewer/src/models/volumeloader.h
@@ -22,10 +22,32 @@ public:
     Volume *buildForCoverPrefetch();
     VolumeBuildResult buildForCoverPrefetchResult();
 
+    /**
+     * @brief build
+     * @param onlyCover specifies to prefetch only the cover page
+     *
+     * Generate Volume asynchronously.
+     */
     static Volume *buildForCoverPrefetchAsync(QString path);
-    Volume *buildForContainingImage();
-    ImageContent loadThumbnailSourceImage();
 
+    /**
+     * @brief buildForContainingImage
+     *
+     * Special initialization method to skip the enumeration of image files
+     * in the Volume and read the first image faster.
+     */
+    Volume *buildForContainingImage();
+
+    /**
+     * @brief thumbnail
+     *
+     * Read and return the image of the youngest file name in Volume
+     */
+    ImageContent loadThumbnailSourceImage();
+    /**
+     * @brief A factory function that returns an instance of IFileVolume from the path of the specified file or directory
+     * @return An object that inherits the IFileVolume interface. It is null if generation failed
+     */
     static Volume *createVolume(QObject *parent, QString path);
     static VolumeBuildResult createVolumeResult(QObject *parent, QString path);
 
--- components/fileloader/fileloaderrararchive.h
+++ components/fileloader/fileloaderrararchive.h
@@ -12,16 +12,48 @@ public:
     FileLoaderRarArchive(QObject *parent, QString rarpath);
     ~FileLoaderRarArchive();
 
+    /**
+     * @brief isArchive
+     * @return return true, if the instance treates an archive file
+     */
     bool isArchive() const override { return true; }
+    /**
+     * @brief isValid
+     * @return return true, if the instance can load images
+     */
     bool isValid() const override { return m_valid; }
     bool hasSubDirectories() const override { return true; }
+    /**
+     * @brief volumePath
+     * @return the path of the instance
+     */
     QString volumePath() const override { return m_volumepath; }
     QString realVolumePath() const override { return volumePath(); }
+    /**
+     * @brief contents
+     * @return all image files without parent path(filename only)
+     */
     QStringList contents() override;
+    /**
+     * @brief subArchives
+     * @return all archive files with in the instance
+     */
     QStringList subArchives() const override { return m_subArchiveList; }
+    /**
+     * @brief getFile get a file specified by filename
+     * @param filename
+     * @param mutex if the method needs to lock resource, must be use the mutex
+     * @return file binary data
+     */
     QByteArray getFile(QString filename, QMutex &mutex) override;
     FileLoadResult getFileResult(QString filename, QMutex &mutex) override;
     ArchiveOpenError archiveOpenError() const override { return m_archiveOpenError; }
+
+    /**
+     * @brief getCacheMode
+     *
+     * Indicates the state when Volume created or has already been decompressed.
+     */
     InflateCacheMode getCacheMode() const override { return InflateNoCached; }
 
 protected:
