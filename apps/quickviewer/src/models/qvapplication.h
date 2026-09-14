3358b386b181e0d6b7a28b685b15277825a357b9
hirano <31331343+nai62@users.noreply.github.com>
2026-09-14 17:27:11 +0900

    Fix

--- apps/quickviewer/src/models/qvapplication.h
+++ apps/quickviewer/src/models/qvapplication.h
@@ -333,8 +333,7 @@ public:
         //QString themeFilePath = getApplicationFilePath(":/themes/"+m_uiTheme+".qss"); //Local files
         QString themeFilePath(":/themes/" + m_uiTheme + ".qss"); // Resource files
         QFile File(themeFilePath);
-        File.open(QFile::ReadOnly);
-        QString styleSheet = QString(File.readAll());
+        const QString styleSheet = File.open(QFile::ReadOnly) ? QString(File.readAll()) : QString();
         QApplication::setStyleSheet(styleSheet);
     }
 
