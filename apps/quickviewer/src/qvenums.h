b5dc34c5bfced5676b471bea92b7ee83e8bdb704
hirano <31331343+nai62@users.noreply.github.com>
2026-09-18 16:38:33 +0900

    Make qvEnums a namespace
    The enum holder does not have to be a QObject: turn it into a namespace with
    Q_NAMESPACE/Q_ENUM_NS. Q_NAMESPACE provides the staticMetaObject that
    QVApplication uses to store and read the values by name, so the call sites
    keep working unchanged.

--- apps/quickviewer/src/qvenums.h
+++ apps/quickviewer/src/qvenums.h
@@ -3,91 +3,92 @@
 
 #include <QtCore>
 
-class qvEnums : public QObject
-{
-    Q_OBJECT
-public:
-    qvEnums(QObject *parent)
-        : QObject(parent)
-    {}
+/**
+ * Enumerations shared by the application. The namespace carries the meta
+ * object so that the values can be stored in and read from the settings by
+ * name.
+ */
+namespace qvEnums {
+Q_NAMESPACE
 
-    enum ShaderEffect {
-        UnPrepared,
-        CpuBicubic,
-        CpuSpline16,
-        CpuSpline36,
-        CpuLanczos3,
-        CpuLanczos4,
+enum ShaderEffect {
+    UnPrepared,
+    CpuBicubic,
+    CpuSpline16,
+    CpuSpline36,
+    CpuLanczos3,
+    CpuLanczos4,
 
-        UsingFixedShader,
-        NearestNeighbor,
-        Bilinear,
+    UsingFixedShader,
+    NearestNeighbor,
+    Bilinear,
 
-        UsingCpuResizer,
-        BilinearAndCpuBicubic,
-        BilinearAndCpuSpline16,
-        BilinearAndCpuSpline36,
-        BilinearAndCpuLanczos,
+    UsingCpuResizer,
+    BilinearAndCpuBicubic,
+    BilinearAndCpuSpline16,
+    BilinearAndCpuSpline36,
+    BilinearAndCpuLanczos,
 
-        UsingSomeShader,
+    UsingSomeShader,
 #ifndef QV_WITHOUT_OPENGL
-        Bicubic,
-        Lanczos
+    Bicubic,
+    Lanczos
 #endif
-    };
-    Q_ENUM(ShaderEffect)
-
-    enum CatalogViewMode {
-        List,
-        Icon,
-        IconNoText,
-    };
-    Q_ENUM(CatalogViewMode)
-
-    enum FolderViewSort {
-        OrderByName,
-        OrderByUpdatedAt,
-    };
-    Q_ENUM(FolderViewSort)
-
-    enum ToolbarIconSize {
-        NormalIcon = 24,
-        LargeIcon = 32,
-        Large2Icon = 40,
-    };
-    Q_ENUM(ToolbarIconSize)
-
-    enum FitMode {
-        NoFitting,
-        FitToRect,
-        FitToWidth
-    };
-    Q_ENUM(FitMode)
-
-    enum SvgLoaderBackend {
-        Resvg,
-        QtSvg,
-    };
-    Q_ENUM(SvgLoaderBackend)
-
-    enum OptionViewOnStartup {
-        NoViewStartup,
-        FolderStartup,
-        CatalogStartup,
-        RetouchStartup,
-        //        ExifStartup,
-    };
-    Q_ENUM(OptionViewOnStartup)
-
-    enum ImageSortBy {
-        SortByFileName,
-        SortByFileNameDescending,
-        SortByFileSize,
-        SortByFileSizeDescending,
-        SortByModifiedTime,
-        SortByModifiedTimeDescending,
-    };
-    Q_ENUM(ImageSortBy)
 };
+Q_ENUM_NS(ShaderEffect)
+
+enum CatalogViewMode {
+    List,
+    Icon,
+    IconNoText,
+};
+Q_ENUM_NS(CatalogViewMode)
+
+enum FolderViewSort {
+    OrderByName,
+    OrderByUpdatedAt,
+};
+Q_ENUM_NS(FolderViewSort)
+
+enum ToolbarIconSize {
+    NormalIcon = 24,
+    LargeIcon = 32,
+    Large2Icon = 40,
+};
+Q_ENUM_NS(ToolbarIconSize)
+
+enum FitMode {
+    NoFitting,
+    FitToRect,
+    FitToWidth
+};
+Q_ENUM_NS(FitMode)
+
+enum SvgLoaderBackend {
+    Resvg,
+    QtSvg,
+};
+Q_ENUM_NS(SvgLoaderBackend)
+
+enum OptionViewOnStartup {
+    NoViewStartup,
+    FolderStartup,
+    CatalogStartup,
+    RetouchStartup,
+    //        ExifStartup,
+};
+Q_ENUM_NS(OptionViewOnStartup)
+
+enum ImageSortBy {
+    SortByFileName,
+    SortByFileNameDescending,
+    SortByFileSize,
+    SortByFileSizeDescending,
+    SortByModifiedTime,
+    SortByModifiedTimeDescending,
+};
+Q_ENUM_NS(ImageSortBy)
+
+} // namespace qvEnums
 
 #endif // QVENUMS_H
